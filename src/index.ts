f73babe2d94a6f706439db92da25985afbfe5cbd
Julian Ceipek <julian.ceipek@gmail.com>
2021-04-17 19:12:22 -0600

    Swarming with interest and danger maps

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,13 @@ class Agents {
     maxSpeed: number[]
     tempGridCellId: number[]
     interestMap: Uint8Array
+    dangerMap: Uint8Array
 
     // One overall, not for each agent
     unitDirections: V2.t[]
 
     constructor(dims: V2.t) {
-        const count = 10;
+        const count = 500;
 
         this.pos = new Array(count);
         this.vel = new Array(count);
@@ -28,9 +29,10 @@ class Agents {
 
         const interestMapResolution = 8;
         this.interestMap = new Uint8Array(count * interestMapResolution);
+        this.dangerMap = new Uint8Array(count * interestMapResolution);
         this.unitDirections = new Array(interestMapResolution);
         for (let i = 0; i < interestMapResolution; i++) {
-            const angle = TAU/i;
+            const angle = i/interestMapResolution * TAU;
             this.unitDirections[i] = V2.fromValues(Math.cos(angle), Math.sin(angle));
         }
 
@@ -50,8 +52,8 @@ class Agents {
 }
 
 class OccupancyGrid {
-    width: number = 100
-    height: number = 100
+    width: number = 10
+    height: number = 10
     maxAgentsPerCell: number = 10
 
     grid: Array<Array<number>>
@@ -63,9 +65,27 @@ class OccupancyGrid {
         }
     }
 
-    addAgent(cellX: number, cellY: number, agentId: number): number {
+    getCellIdx(cellX: number, cellY: number): number {
         const cellIdx = cellX + cellY * this.width;
-        this.grid[cellIdx].push(agentId);
+        return cellIdx;
+    }
+
+    getCell(cellX: number, cellY: number): number[] | null {
+        const cellIdx = this.getCellIdx(cellX, cellY);
+        const val = this.grid[cellIdx];
+        if (val) {
+            return val;
+        } else {
+            return null;
+        }
+    }
+
+    addAgent(cellX: number, cellY: number, agentId: number): number {
+        const cellIdx = this.getCellIdx(cellX, cellY);
+        const agents = this.grid[cellIdx];
+        if (agents.length < this.maxAgentsPerCell) {
+            agents.push(agentId);
+        }
         return cellIdx;
     }
 
@@ -80,18 +100,23 @@ class OccupancyGrid {
 const agentState = new Agents(V2.fromValues(1000,1000));
 const occupancyGrid = new OccupancyGrid();
 
-let lastElapsed = 0;
+let xxx = false;
+
 function renderLoop(elapsed: number) {
-    console.log('FRAME');
-    CTX.onFrameStart(elapsed * 0.001);
-    const dims = CTX.rendererGL.dims;
-    const dt = CTX.dt;
     const D2D = CTX.D2D;
     const W2D = CTX.W2D;
     const UI = CTX.UI;
-    lastElapsed = elapsed;
-
+    CTX.onFrameStart(elapsed * 0.001);
+    const dims = CTX.rendererGL.dims;
+    const dt = CTX.dt;
     D2D.clearScreen();
+
+    // UI.checkboxBare('XXX', 500, 500, false, 20);
+    D2D.setStrokeColor(0xFFFFFFFF);
+    // D2D.strokeRoundedRect(500, 500, 15, 15, 3);
+    // D2D.strokeCenterRect(500, 500, 100, 100);
+    // D2D.strokeRect(500, 500, 100, 100);
+
     
     W2D.setStrokeColor(0xFFFFFFFF);
     {
@@ -99,34 +124,83 @@ function renderLoop(elapsed: number) {
         const target = V2.fromValues(mousePos[0], dims[1] - mousePos[1]);
         W2D.strokeCrosshair(target[0], target[1]);
 
+        W2D.setStrokeThickness(1);
+        W2D.setStrokeColor(0xFFFF00FF);
         const count = agentState.count;
         const pos = agentState.pos;
         const tempGridCellId = agentState.tempGridCellId;
+
+        const cellW = dims[0]/(occupancyGrid.width-1);
+        const cellH = dims[1]/(occupancyGrid.height-1);
+        // Populate grid cells with agents:
         for (let i = 0; i < count; i++) {
             const px = pos[i][0];
             const py = pos[i][1];
-            const cellX = Math.floor(lerp(0, occupancyGrid.width-1, clamp01(px/dims[0])));
-            const cellY = Math.floor(lerp(0, occupancyGrid.height-1, clamp01(py/dims[1])));
+            const cellX = Math.floor(px/dims[0] * (occupancyGrid.width-1) + 0.5);
+            const cellY = Math.floor(py/dims[1] * (occupancyGrid.height-1) + 0.5);
+            if (cellX < 0 || cellY < 0 ||  cellX >= occupancyGrid.width || cellY >= occupancyGrid.height) continue;
             tempGridCellId[i] = occupancyGrid.addAgent(cellX, cellY, i);
+            // Draw cell where agent is:
+            // W2D.strokeCenterRect(cellX * cellW, cellY * cellH, cellW, cellH);
         }
+
+        // {// Draw Grid
+        //     W2D.setStrokeColor(0xFF0000FF);
+        //     for (let x = 0; x < occupancyGrid.width; x++) {
+        //         for (let y = 0; y < occupancyGrid.height; y++) {
+        //             // W2D.strokeCenterRect(x * cellW, y * cellH, cellW, cellH);
+        //         }            
+        //     }
+        // }
+
+        // { // Draw cell mouse is in
+        //     W2D.setStrokeColor(0x00FF00FF);
+        //     W2D.setFillColor(0x00FF00FF);
+        //     const px = target[0];
+        //     const py = target[1];
+        //     const cellX = Math.floor(px/dims[0] * (occupancyGrid.width-1) + 0.5);
+        //     const cellY = Math.floor(py/dims[1] * (occupancyGrid.height-1) + 0.5);
+        //     W2D.strokeCenterRect(cellX * cellW, cellY * cellH, cellW, cellH);
+        //     W2D.fillText(px+30, py, `${cellX} - ${cellY}`)
+        // }
+
 
         const radius = 50;
         const sqrRadius = radius * radius;
-        W2D.setStrokeColor(0xFF0000FF);
+        
         for (let i = 0; i < count; i++) {
             const currPos = pos[i];
-            for (let j = 0; j < count; j++) {
-                const cmpPos = pos[j];
-                if (V2.sqrDist(currPos, cmpPos) < sqrRadius) {
-                    W2D.strokeLine(currPos[0], currPos[1], cmpPos[0], cmpPos[1]);
-                }
-            }
+            // { // Draw close agents (naive)
+            //     W2D.setStrokeColor(0xFF0000FF);
+            //     for (let j = 0; j < count; j++) {
+            //         const cmpPos = pos[j];
+            //         if (V2.sqrDist(currPos, cmpPos) < sqrRadius) {
+            //             W2D.strokeLine(currPos[0], currPos[1], cmpPos[0], cmpPos[1]);
+            //         }
+            //     }
+            // }
 
-            // const px = pos[i][0];
-            // const py = pos[i][1];
-            // const cellX = Math.floor(lerp(0, occupancyGrid.width-1, clamp01(px/dims[0])));
-            // const cellY = Math.floor(lerp(0, occupancyGrid.height-1, clamp01(py/dims[1])));
-            // tempGridCellId[i] = occupancyGrid.addAgent(cellX, cellY, i);
+            // { // Draw close agents (grid)
+            //     W2D.setStrokeColor(0x00FF00FF);
+            //     const px = pos[i][0];
+            //     const py = pos[i][1];
+            //     const cellX = Math.floor(px/dims[0] * (occupancyGrid.width-1) + 0.5);
+            //     const cellY = Math.floor(py/dims[1] * (occupancyGrid.height-1) + 0.5);
+            //     for (let x = cellX-1; x <= cellX + 1; x++) {
+            //         for (let y = cellY-1; y <= cellY + 1; y++) {
+            //             const agentIDs = occupancyGrid.getCell(x, y);
+            //             if (agentIDs === null) { continue; }
+            //             for (let agentIdx = 0; agentIdx < agentIDs.length; agentIdx++) {
+            //                 const agentId = agentIDs[agentIdx];
+            //                 if (agentId === i) { continue; }
+            //                 const cmpPos = pos[agentId];
+            //                 if (V2.sqrDist(currPos, cmpPos) < sqrRadius) {
+            //                     W2D.strokeLine(currPos[0], currPos[1], cmpPos[0], cmpPos[1]);
+            //                 }
+            //             }
+            //         }   
+            //     }
+            // }
         }
 
         simulateAgents(CTX, agentState, target);
@@ -142,6 +216,7 @@ function renderLoop(elapsed: number) {
 window.requestAnimationFrame(renderLoop);
 
 function simulateAgents(ctx: Context, agentState: Agents, target: V2.t) {
+    const dims = ctx.rendererCanvas.dims;
     const dt = ctx.dt;
     const W2D = ctx.W2D;
     const count = agentState.count;
@@ -150,33 +225,96 @@ function simulateAgents(ctx: Context, agentState: Agents, target: V2.t) {
     const facing = agentState.facing;
     const maxSpeed = agentState.maxSpeed;
     for (let i = 0; i< count; i++) {
+        // Move agent based on velocity
         pos[i] = V2.add(pos[i], V2.scale(vel[i], dt));
         const len = V2.length(vel[i]);
         if (len > 0) {
-            // Normalize
+            // Normalize; update facing direction to point in velocity direction
             facing[i] = V2.scale(vel[i], 1/len);
         }
     
-
+        // Calculating the interest map (how good is it to move in a given direction)
         const agentToTargetNorm = V2.sub(target,pos[i]);
         V2.normalize(agentToTargetNorm);
         const resolution = agentState.unitDirections.length;
+        // W2D.setStrokeColor(0x00FF00FF);
         for (let j = 0; j < resolution; j++) {
             const dir = agentState.unitDirections[j];
             const goodness = clamp01(V2.dot(agentToTargetNorm, dir));
             agentState.interestMap[resolution*i + j] = (goodness * 255)|0;
-            const visScale = 100;//goodness * 100;
-            W2D.strokeLine(pos[i][0], pos[i][1], pos[i][0] + dir[0] * visScale, pos[i][1] + dir[1] * visScale);
+            const visScale = goodness * 100;
+            // W2D.strokeLine(pos[i][0], pos[i][1], pos[i][0] + dir[0] * visScale, pos[i][1] + dir[1] * visScale);
+        }
+        
+        // Calculate the danger map (how bad it is to move in a given direction)
+        // W2D.setStrokeColor(0xFF0000FF);
+        for (let j = 0; j < resolution; j++) {
+            const dir = agentState.unitDirections[j];
+            let badness = 0;
+            const threshold = 150;
+            // Naive method:
+            // for (let k = 0; k < count; k++) {
+            //     if (i === k) { continue; }
+            //     const obstaclePos = pos[k];
+            //     let agentToObstacleNorm = V2.sub(obstaclePos, pos[i]);
+            //     const dist = V2.length(agentToObstacleNorm);
+            //     agentToObstacleNorm = V2.scale(agentToObstacleNorm, 1/dist);
+            //     const dP = V2.dot(dir, agentToObstacleNorm);
+            //     badness += dP * Math.max(0, (threshold - dist)/threshold);
+            // }
+
+            const px = pos[i][0];
+            const py = pos[i][1];
+            const cellX = Math.floor(px/dims[0] * (occupancyGrid.width-1) + 0.5);
+            const cellY = Math.floor(py/dims[1] * (occupancyGrid.height-1) + 0.5);
+            const currPos = pos[i];
+            for (let x = cellX-1; x <= cellX + 1; x++) {
+                for (let y = cellY-1; y <= cellY + 1; y++) {
+                    const agentIDs = occupancyGrid.getCell(x, y);
+                    if (agentIDs === null) { continue; }
+                    for (let agentIdx = 0; agentIdx < agentIDs.length; agentIdx++) {
+                        const agentId = agentIDs[agentIdx];
+                        if (agentId === i) { continue; }
+                        const obstaclePos = pos[agentId];
+                        // if (V2.sqrDist(currPos, obstaclePos) < threshold) {
+                        //     W2D.strokeLine(currPos[0], currPos[1], obstaclePos[0], obstaclePos[1]);
+                        // }
+                        let agentToObstacleNorm = V2.sub(obstaclePos, pos[i]);
+                        const dist = V2.length(agentToObstacleNorm);
+                        agentToObstacleNorm = V2.scale(agentToObstacleNorm, 1/dist);
+                        const dP = V2.dot(dir, agentToObstacleNorm);
+                        badness += dP * Math.max(0, (threshold - dist)/threshold);
+                    }
+                }   
+            }
+
+            badness = clamp01(badness);
+            agentState.dangerMap[resolution*i + j] = (badness * 255)|0;
+            const visScale = badness * 100;
+            // W2D.strokeLine(pos[i][0], pos[i][1], pos[i][0] + dir[0] * visScale, pos[i][1] + dir[1] * visScale);
         }
 
+        // W2D.setStrokeColor(0x0000FFFF);
+        let x = 0;
+        let y = 0;
+        for (let j = 0; j < resolution; j++) {
+            const dir = agentState.unitDirections[j];
+            const goodness = agentState.interestMap[resolution*i + j];
+            const badness = agentState.dangerMap[resolution*i + j];
+            const actual = Math.max(0, goodness - badness)/255;
+            x += dir[0] * actual;
+            y += dir[1] * actual;
+        }
+        const goalNorm = V2.fromValues(x, y); V2.normalize(goalNorm);
+        const visScale = 100;
+        // W2D.strokeLine(pos[i][0], pos[i][1], pos[i][0] + goalNorm[0] * visScale, pos[i][1] + goalNorm[1] * visScale);
+
         // const distToTarget = V2.length(agentToTarget);
-        // const speed = lerp(0, maxSpeed[i], clamp01(ilerp(50, 200, distToTarget)));
-        // const desiredVel = V2.scale(V2.normalized(agentToTarget), speed);
-        // const steering = V2.sub(desiredVel, vel[i]); // this is still a velocity
+        const speed = maxSpeed[i];//lerp(0, maxSpeed[i], clamp01(ilerp(50, 200, distToTarget)));
+        const desiredVel = V2.scale(goalNorm, speed);
+        const steering = V2.sub(desiredVel, vel[i]); // this is still a velocity
         // // Force m/s/s
-        // vel[i] = V2.add(vel[i], V2.scale(steering, dt));
-
-
+        vel[i] = V2.add(vel[i], V2.scale(steering, dt));
 
 
         // const agentToTarget = V2.sub(target,pos[i]);
